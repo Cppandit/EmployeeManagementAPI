11ac6c00c71d547015b8059342eb07cfebb7d218
Cppandit <154009758+Cppandit@users.noreply.github.com>
2026-06-11 11:57:59 +0530

    Add project files.

---
+++ EmployeeManagementAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> employees = new();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            employees.Add(employee);
+            return Ok(employee);
+        }
+    }
+}
